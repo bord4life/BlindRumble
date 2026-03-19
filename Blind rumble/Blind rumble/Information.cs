4c9d783c8be7f2a7ae7e76525a664077d33664d7
Bord <bordsyoyos@gmail.com>
2026-03-18 19:24:32 -0500

    current progress

---
+++ Blind rumble/Blind rumble/Information.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlindRumble
+{
+    internal class Information
+    {
+        public static class BuildInfo
+        {
+            // The use of this class should be obvious
+            public const string ModName = "BlindRumble";
+            public const string ModVersion = "1.0.";
+            public const string Description = "Changes the vision to be sonar/sound based";
+            public const string Author = "MadLike";
+            public const string Company = "";
+        }
+
+    }
+}
