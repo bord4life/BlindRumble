9b4df41603e6b98f67a15e74ea8c3ab084b7a6a8
Bord <bordsyoyos@gmail.com>
2026-03-20 14:06:36 -0500

    0.3.0
    NET. 10 Version

---
+++ GameDetector.cs
@@ -0,0 +1,27 @@
+﻿using Il2CppRUMBLE.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using HarmonyLib;
+
+namespace BlindRumble
+{
+    internal class GameDetector
+    {
+        [HarmonyPatch(typeof(Il2CppRUMBLE.Environment.Matchmaking.MatchmakeConsole), "MatchmakeStatusUpdated", new Type[] { typeof(MatchmakingHandler.MatchmakeStatus), typeof(bool) })]
+        public static class Patch1
+        {
+            private static void Prefix(GameObject __instance, MatchmakingHandler.MatchmakeStatus status, bool instantLeverStep)
+            {
+
+                if (status == MatchmakingHandler.MatchmakeStatus.Success)
+                {
+                    SonarMode.matchFound = true;
+                }
+            }
+        }
+    }
+}
